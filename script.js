674b2bc03e4c2427306522be6e0128f50a526dc1
Barathga <96562210+Barathga@users.noreply.github.com>
2022-01-23 12:24:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+//1st program
+// function maximum(array)
+// {
+//     var val = array[0];
+//     for (var i = 1; i < array.length; i++)
+//     {
+//         if (array[i] > val) 
+//         val = array[i];
+//     }
+//     return val;
+// }
+// a = maximum([10, 21, 35, 11, 50])
+// console.log(a);
+
+
+//2nd program
+// function minimum(array)
+// {
+//     var val = array[0]
+//     for(i=1; i<array.length; i++)
+//     {
+//         if(array[i] < val)
+//         val = array[i]
+//     }
+//     return val
+// }
+// a = minimum([17,10,1999,7,1,2004])
+// console.log(a)
+
+
+//3rd program
+// arr = [1, 2, 3, 4];
+// arr1 = [];
+// for (let i = arr.length - 1; i >= 0; i--)
+// {
+//     arr1.push(arr[i]);
+// }
+// console.log(arr1);
+
+
+//4th program
+// function addition()
+// {
+//     var a = 5;
+//     var b = 3;
+//     var c = a + b;
+//     console.log(c)
+// }
+//  addition();
+
+
+//5th program
+// function power()
+// {
+//     var a = 2;
+//     var b = 3;
+//     var c = a**b
+//     console.log(c)
+// }
+// power()
